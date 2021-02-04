31793a73aedd9401c8593f38b8ba6ef8a002e4fe
Max Kimlyk <maxskater3@yandex.ru>
2021-02-04 17:48:38 +0300

    docker compose

--- run_test_docker.sh
+++ run_test_docker.sh
@@ -2,4 +2,5 @@
 
 export TELEGRAM_API_TOKEN="334131527:AAHPUaJNEX39HOkTRAptWs2ccQ9Vgv58z3M"
 export TELEGRAM_USER_ID="187193654"
-docker build -t bitrthday-lord-bot ./ && docker run -it --rm --name bitrthday-lord-bot --mount type=bind,source=$PWD/cache,target=/var/cache --env TELEGRAM_API_TOKEN=$TELEGRAM_API_TOKEN --env TELEGRAM_USER_ID=$TELEGRAM_USER_ID bitrthday-lord-bot
+# docker build -t bitrthday-lord-bot ./ && docker run -it --rm --name bitrthday-lord-bot --mount type=bind,source=$PWD/cache,target=/var/cache --env TELEGRAM_API_TOKEN=$TELEGRAM_API_TOKEN --env TELEGRAM_USER_ID=$TELEGRAM_USER_ID bitrthday-lord-bot
+docker-compose up --build
